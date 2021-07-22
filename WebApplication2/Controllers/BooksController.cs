ce8804d7ddce24e3fd543c21d0cd6ec32d30fae7
moazbasheer <moazbasheer97@gmail.com>
2021-07-22 10:04:31 +0200

    initial commit

---
+++ WebApplication2/Controllers/BooksController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class BooksController : Controller
+    {
+
+        public readonly ApplicationDbContext _db;
+        [BindProperty]
+        public Book Book { set; get; }
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+           return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Book = new Book();
+            if (id == null)
+            {
+                // insert
+                return View(Book);
+            }
+            // update
+            Book = _db.Books.Find(id);
+            if(Book == null)
+            {
+                return NotFound();
+            }
+            return View(Book);
+        }
+
+        [HttpPost]
+        public IActionResult Upsert()
+        {
+            if(ModelState.IsValid)
+            {
+                if(Book.Id == 0) // insert
+                {
+                    _db.Books.Add(Book);
+                }
+                else
+                {
+                    _db.Books.Update(Book);
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Book);
+        }
+
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var book = await _db.Books.FindAsync(id);
+            if(book == null)
+            {
+                return Json(new { success = false, message = "Not found" });
+            }
+            _db.Books.Remove(book);
+            await _db.SaveChangesAsync();
+            return Json(new { success=true, message="successfully deleted" });
+        }
+        #endregion
+    }
+}
---
+++ WebApplication2/Models/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { set; get; }
+        [Required]
+        public string Name{ set; get; }
+        public string Author { set; get; }
+        public string ISBN { set; get; }
+    }
+}
